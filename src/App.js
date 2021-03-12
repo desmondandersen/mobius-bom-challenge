725f9597bec81aa5323b493d9950f2f9443040b3
Desmond Andersen <dfa@ucla.edu>
2021-03-12 03:38:41 -0800

    Moved Modal from App to BOM

--- src/App.js
+++ src/App.js
@@ -16,36 +16,6 @@ class App extends Component {
     super(props);
     this.state = {
       bomIdList: [],
-      showModal: false,
-      bomId: '',
-      partNumber: '',
-      cost: '',
-      quantity: '',
-    };
-    this.handleAddPart = (id) => {
-      this.setState({ showModal: true });
-      this.setState({ bomId: id });
-    };
-    this.handleClose = () => {
-      this.setState({ showModal: false });
-    };
-    this.handleChange = (e) => {
-      const name = e.target.name;
-      const val = e.target.value;
-      this.setState({ [name]: val });
-    };
-    this.handleSubmit = (e) => {
-      e.preventDefault();
-      console.log('Submitting...');
-      const item = {
-        specific_part: this.state.partNumber,
-        item_unit_cost: this.state.cost,
-        quantity: this.state.quantity,
-      };
-      API.post(`bom/${this.state.bomId}/bomitem`, item).then((res) => {
-        console.log('Response: ' + res);
-      });
-      this.handleClose();
     };
   }
 
@@ -59,68 +29,19 @@ class App extends Component {
 
   render() {
     return (
-      <>
-        <div className='app'>
-          <h1>Bill of Materials</h1>
-          <div className='bom-container'>
-            {
-              // Create a Product component for each object in the BOM
-              this.state.bomIdList.map((bom, key) => {
-                return (
-                  <BOM
-                    id={bom.id}
-                    key={key}
-                    handleAddPart={this.handleAddPart}
-                  />
-                );
-              })
-            }
-          </div>
-
-          <Modal show={this.state.showModal} onHide={this.handleClose}>
-            <Modal.Header closeButton>
-              <Modal.Title>Add New Part</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-              <Form onSubmit={this.handleSubmit}>
-                <Form.Group controlId='part'>
-                  <Form.Label>Part ID</Form.Label>
-                  <Form.Control
-                    type='text'
-                    name='partNumber'
-                    placeholder='Enter part number'
-                    onChange={this.handleChange}
-                    required
-                  />
-                </Form.Group>
-                <Form.Group controlId='quantity'>
-                  <Form.Label>Quantity</Form.Label>
-                  <Form.Control
-                    type='number'
-                    name='quantity'
-                    placeholder='Enter quantity'
-                    onChange={this.handleChange}
-                    required
-                  />
-                </Form.Group>
-                <Form.Group controlId='price'>
-                  <Form.Label>Unit Price</Form.Label>
-                  <Form.Control
-                    type='number'
-                    name='cost'
-                    placeholder='Enter price (in cents)'
-                    onChange={this.handleChange}
-                    required
-                  />
-                </Form.Group>
-                <Button variant='primary' type='submit'>
-                  Confirm
-                </Button>
-              </Form>
-            </Modal.Body>
-          </Modal>
+      <div className='app'>
+        <h1>Bill of Materials</h1>
+        <div className='bom-container'>
+          {
+            // Create a Product component for each object in the BOM
+            this.state.bomIdList.map((bom, key) => {
+              return (
+                <BOM id={bom.id} key={key} handleAddPart={this.handleAddPart} />
+              );
+            })
+          }
         </div>
-      </>
+      </div>
     );
   }
 }
