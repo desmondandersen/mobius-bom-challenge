b63f53a98f6663d84e95fc292e7bb960f9ea2826
Desmond Andersen <dfa@ucla.edu>
2021-03-12 03:37:01 -0800

    New parts will show up without refreshing

--- src/components/BOM.js
+++ src/components/BOM.js
@@ -6,6 +6,8 @@ import API from '../api';
 import Card from 'react-bootstrap/Card';
 import ListGroup from 'react-bootstrap/ListGroup';
 import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import Form from 'react-bootstrap/Form';
 
 // Import components
 import Item from './Item';
@@ -16,9 +18,36 @@ class BOM extends Component {
     super(props);
     this.state = {
       bomItems: [],
+      showModal: false,
+      partNumber: '',
+      cost: '',
+      quantity: '',
     };
-    this.handleClick = () => {
-      this.props.handleAddPart(this.props.id);
+    this.handleOpenModal = () => {
+      this.setState({ showModal: true });
+    };
+    this.handleCloseModal = () => {
+      this.setState({ showModal: false });
+    };
+    this.handleChange = (e) => {
+      const name = e.target.name;
+      const val = e.target.value;
+      this.setState({ [name]: val });
+    };
+    this.handleSubmit = (e) => {
+      e.preventDefault();
+      console.log('Submitting...');
+      let item = {
+        specific_part: this.state.partNumber,
+        item_unit_cost: this.state.cost,
+        quantity: this.state.quantity,
+      };
+      API.post(`bom/${this.props.id}/bomitem`, item).then((res) => {
+        item = res.data;
+        const newBomItems = this.state.bomItems.concat(item);
+        this.setState({ bomItems: newBomItems });
+      });
+      this.handleCloseModal();
     };
     this.handleRemovePart = (itemId) => {
       API.delete(`bom/${this.props.id}/bomitem/${itemId}`);
@@ -44,27 +73,72 @@ class BOM extends Component {
 
   render() {
     return (
-      <Card bg='light' className='bom-card'>
-        <Card.Header>BOM {this.props.id}</Card.Header>
-        <ListGroup variant='flush'>
-          {this.state.bomItems.map((item, key) => (
-            <ListGroup.Item key={key}>
-              <Item
-                id={item.id}
-                part={item.specific_part}
-                quantity={item.quantity}
-                cost={item.item_unit_cost}
-                handleRemovePart={this.handleRemovePart}
-              />
-            </ListGroup.Item>
-          ))}
-        </ListGroup>
-        <Card.Body>
-          <Button onClick={this.handleClick} variant='info'>
-            Add New Part
-          </Button>
-        </Card.Body>
-      </Card>
+      <>
+        <Card bg='light' className='bom-card'>
+          <Card.Header>BOM {this.props.id}</Card.Header>
+          <ListGroup variant='flush'>
+            {this.state.bomItems.map((item, key) => (
+              <ListGroup.Item key={key}>
+                <Item
+                  id={item.id}
+                  part={item.specific_part}
+                  quantity={item.quantity}
+                  cost={item.item_unit_cost}
+                  handleRemovePart={this.handleRemovePart}
+                />
+              </ListGroup.Item>
+            ))}
+          </ListGroup>
+          <Card.Body>
+            <Button onClick={this.handleOpenModal} variant='info'>
+              Add New Part
+            </Button>
+          </Card.Body>
+        </Card>
+
+        <Modal show={this.state.showModal} onHide={this.handleCloseModal}>
+          <Modal.Header closeButton>
+            <Modal.Title>Add New Part</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Form onSubmit={this.handleSubmit}>
+              <Form.Group controlId='part'>
+                <Form.Label>Part ID</Form.Label>
+                <Form.Control
+                  type='text'
+                  name='partNumber'
+                  placeholder='Enter part number'
+                  onChange={this.handleChange}
+                  required
+                />
+              </Form.Group>
+              <Form.Group controlId='quantity'>
+                <Form.Label>Quantity</Form.Label>
+                <Form.Control
+                  type='number'
+                  name='quantity'
+                  placeholder='Enter quantity'
+                  onChange={this.handleChange}
+                  required
+                />
+              </Form.Group>
+              <Form.Group controlId='price'>
+                <Form.Label>Unit Price</Form.Label>
+                <Form.Control
+                  type='number'
+                  name='cost'
+                  placeholder='Enter price (in cents)'
+                  onChange={this.handleChange}
+                  required
+                />
+              </Form.Group>
+              <Button variant='primary' type='submit'>
+                Confirm
+              </Button>
+            </Form>
+          </Modal.Body>
+        </Modal>
+      </>
     );
   }
 }
